00509de70d1c96c58cbdcb35d0f94f169bfe64a0
Rohan <rohan.seth497@gmail.com>
2018-10-07 09:33:34 +0530

    need to add custom type objects

--- server/src/resources/Util/nodeDefinitions.js
+++ server/src/resources/Util/nodeDefinitions.js
@@ -9,6 +9,12 @@ import {
   MessageQuery
 } from '../Query/index'
 
+import {
+  GraphQLMessage,
+  GraphQLThread,
+  GraphQLUser
+} from '../graphQLTypes'
+
 const { nodeInterface, nodeField } = nodeDefinitions(
   globalId => {
     const { type, id } = fromGlobalId(globalId)
@@ -23,6 +29,13 @@ const { nodeInterface, nodeField } = nodeDefinitions(
   },
   obj => {
     // if(obj instanceof )
+    if(obj instanceof 'Message') {
+      return GraphQLMessage;
+    } else if(obj instanceof 'Thread') {
+      return GraphQLThread;
+    } else if(obj instanceof 'User') {
+      return GraphQLUser
+    }
     return null;
   }
 )
